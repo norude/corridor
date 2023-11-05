c6e6f944e78e9aef0823e0f28f4b65bac32c3fef
s <fedor.tryfanau@gmail.com>
2023-11-06 01:29:28 +0300

    added the rest of game's logic, refactored code

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,17 @@
-use std::fmt::Display;
+use core::fmt::Display;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum PlayerColor {
     White,
     Black,
+}
+impl Display for PlayerColor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::White => write!(f, "white"),
+            Self::Black => write!(f, "black"),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -24,14 +32,6 @@ impl Direction {
     }
 }
 
-impl PlayerColor {
-    const fn repr(&self) -> &str {
-        match self {
-            Self::White => " W ",
-            Self::Black => " B ",
-        }
-    }
-}
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Axis {
     Horizontal,
@@ -43,115 +43,163 @@ pub struct Board {
     fences: [[Option<Axis>; 8]; 8],
     black_pawn: (usize, usize),
     white_pawn: (usize, usize),
+    fences_left_for_white: u32,
+    fences_left_for_black: u32,
 }
 
 impl Default for Board {
     fn default() -> Self {
         Self {
             squares: {
-                let mut a: [[Option<PlayerColor>; 9]; 9] = Default::default();
-                a[0][4] = Some(PlayerColor::Black);
-                a[8][4] = Some(PlayerColor::White);
-                a
+                let mut board: [[Option<PlayerColor>; 9]; 9] = Default::default();
+                board[0][4] = Some(PlayerColor::Black);
+                board[8][4] = Some(PlayerColor::White);
+                board
             },
             fences: Default::default(),
             black_pawn: (4, 0),
             white_pawn: (4, 8),
+            fences_left_for_black: 10,
+            fences_left_for_white: 10,
         }
     }
 }
 impl Display for Board {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "╭───┬───┬───┬───┬───┬───┬───┬───┬───╮")?;
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        writeln!(f, "   ╭───┬───┬───┬───┬───┬───┬───┬───┬───╮")?;
         for row_idx in 0..9 {
-            write!(f, "│")?;
+            write!(f, "   │")?;
             self.squares[row_idx]
                 .iter()
                 .enumerate()
                 .try_for_each(|(idx, sq)| {
-                    write!(
-                        f,
-                        "{}{}",
-                        sq.as_ref().map_or("   ", PlayerColor::repr),
-                        if idx == 8 {
-                            "│"
-                        } else if row_idx < 8 && self.fences[row_idx][idx] == Some(Axis::Vertical)
-                            || row_idx > 0 && self.fences[row_idx - 1][idx] == Some(Axis::Vertical)
-                        {
-                            "┃"
-                        } else {
-                            "│"
-                        }
-                    )
-                });
+                    let square_fmt = sq.as_ref().map_or("   ", |c| match c {
+                        PlayerColor::White => " W ",
+                        PlayerColor::Black => " B ",
+                    });
+                    let bar_fmt = if idx != 8
+                        && (row_idx < 8 && self.fences[row_idx][idx] == Some(Axis::Vertical)
+                            || row_idx > 0 && self.fences[row_idx - 1][idx] == Some(Axis::Vertical))
+                    {
+                        "┃"
+                    } else {
+                        "│"
+                    };
+                    write!(f, "{square_fmt}{bar_fmt}")
+                })?;
             writeln!(f)?;
             if row_idx == 8 {
                 break;
             }
-            write!(f, "├")?;
+            write!(f, " {}─┼", row_idx + 1)?;
             (0..9).try_for_each(|idx| {
-                write!(
-                    f,
-                    "{}{}",
-                    if idx < 8 && self.fences[row_idx][idx] == Some(Axis::Horizontal)
-                        || idx > 0 && self.fences[row_idx][idx - 1] == Some(Axis::Horizontal)
-                    {
-                        "━━━"
-                    } else {
-                        "───"
-                    },
-                    if idx == 8 {
-                        "│"
-                    } else {
-                        match self.fences[row_idx][idx] {
-                            Some(Axis::Vertical) => "╂",
-                            Some(Axis::Horizontal) => "┿",
-                            _ => "┼",
-                        }
+                let square_side_fmt = if idx < 8
+                    && self.fences[row_idx][idx] == Some(Axis::Horizontal)
+                    || idx > 0 && self.fences[row_idx][idx - 1] == Some(Axis::Horizontal)
+                {
+                    "━━━"
+                } else {
+                    "───"
+                };
+                let square_corner_fmt = if idx == 8 {
+                    "┤"
+                } else {
+                    match self.fences[row_idx][idx] {
+                        Some(Axis::Vertical) => "╂",
+                        Some(Axis::Horizontal) => "┿",
+                        _ => "┼",
                     }
-                )
-            });
+                };
+                write!(f, "{square_side_fmt}{square_corner_fmt}",)
+            })?;
             writeln!(f)?;
         }
-        writeln!(f, "╰───┴───┴───┴───┴───┴───┴───┴───┴───╯")?;
+        writeln!(f, "   ╰───┼───┼───┼───┼───┼───┼───┼───┼───╯")?;
+        writeln!(f, "       A   B   C   D   E   F   G   H    ")?;
+        writeln!(f)?;
+        writeln!(
+            f,
+            "   White - {:>2} fences │ Black - {:>2} fences",
+            self.fences_left_for_white, self.fences_left_for_black
+        )?;
         Ok(())
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum MovePawnFail {
+pub enum PawnMoveFail {
     PathObstructed,
     NoSecondaryDirection,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum AddFencelFail {
+pub enum AddFenceFail {
     Collides,
     NoPathRemaining,
+    NoFencesRemaining,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MoveMakeFail {
+    AddFenceFail(AddFenceFail),
+    PawnMoveFail(PawnMoveFail),
 }
 
 impl Board {
+    pub fn make_move(&mut self, r#move: Move, player: PlayerColor) -> Result<(), MoveMakeFail> {
+        match r#move {
+            Move::MovePlayer(dir, second_dir) => self
+                .move_pawn(player, dir, second_dir)
+                .map_err(MoveMakeFail::PawnMoveFail),
+            Move::PlaceFence(axis, pos) => self
+                .move_fence(player, axis, pos)
+                .map_err(MoveMakeFail::AddFenceFail),
+        }
+    }
+
+    pub fn is_move_legal(&mut self, r#move: Move, player: PlayerColor) -> Result<(), MoveMakeFail> {
+        match r#move {
+            Move::MovePlayer(dir, second_dir) => self
+                .pawn_move_destination(player, dir, second_dir)
+                .map_err(MoveMakeFail::PawnMoveFail)
+                .map(|_| ()),
+            Move::PlaceFence(axis, pos) => self
+                .is_fence_move_legal(player, axis, pos)
+                .map_err(MoveMakeFail::AddFenceFail),
+        }
+    }
+
     pub fn move_pawn(
         &mut self,
         pawn: PlayerColor,
         dir: Direction,
         second_dir: Option<Direction>,
-    ) -> Result<(), MovePawnFail> {
-        use MovePawnFail::{NoSecondaryDirection, PathObstructed};
+    ) -> Result<(), PawnMoveFail> {
         let (x, y) = match pawn {
             PlayerColor::White => self.white_pawn,
             PlayerColor::Black => self.black_pawn,
         };
+        let (x1, y1) = self.pawn_move_destination(pawn, dir, second_dir)?;
+        assert_eq!(self.squares[y1][x1], None);
+        self.squares[y1][x1] = Some(pawn);
+        self.squares[y][x] = None;
+        match pawn {
+            PlayerColor::White => self.white_pawn = (x1, y1),
+            PlayerColor::Black => self.black_pawn = (x1, y1),
+        };
+        Ok(())
+    }
 
-        let move_with_pawn_no_checks = |s: &mut Self, x1: usize, y1: usize| {
-            assert_eq!(s.squares[y1][x1], None);
-            s.squares[y1][x1] = Some(pawn);
-            s.squares[y][x] = None;
-            match pawn {
-                PlayerColor::White => s.white_pawn = (x1, y1),
-                PlayerColor::Black => s.black_pawn = (x1, y1),
-            };
-            Ok::<(), MovePawnFail>(())
+    pub fn pawn_move_destination(
+        &self,
+        pawn: PlayerColor,
+        dir: Direction,
+        second_dir: Option<Direction>,
+    ) -> Result<(usize, usize), PawnMoveFail> {
+        use PawnMoveFail::{NoSecondaryDirection, PathObstructed};
+        let (x, y) = match pawn {
+            PlayerColor::White => self.white_pawn,
+            PlayerColor::Black => self.black_pawn,
         };
 
         if self.is_obstructed((x, y), dir) {
@@ -160,12 +208,12 @@ impl Board {
 
         let (x1, y1) = dir.offset((x, y));
         if self.squares[y1][x1].is_none() {
-            return move_with_pawn_no_checks(self, x1, y1);
+            return Ok((x1, y1));
         }
 
         if !self.is_obstructed((x1, y1), dir) {
             let (x2, y2) = dir.offset((x1, y1));
-            return move_with_pawn_no_checks(self, x2, y2);
+            return Ok((x2, y2));
         }
 
         let Some(sec_dir) = second_dir else {
@@ -175,8 +223,7 @@ impl Board {
         if self.is_obstructed((x1, y1), sec_dir) {
             return Err(PathObstructed);
         }
-        let (x2, y2) = sec_dir.offset((x1, y1));
-        move_with_pawn_no_checks(self, x2, y2)
+        Ok(sec_dir.offset((x1, y1)))
     }
 
     pub fn is_obstructed(&self, (x, y): (usize, usize), dir: Direction) -> bool {
@@ -204,9 +251,37 @@ impl Board {
         }
     }
 
-    pub fn add_fence(&mut self, axis: Axis, (x, y): (usize, usize)) -> Result<(), AddFencelFail> {
-        use AddFencelFail::{Collides, NoPathRemaining};
+    pub fn move_fence(
+        &mut self,
+        player: PlayerColor,
+        axis: Axis,
+        (x, y): (usize, usize),
+    ) -> Result<(), AddFenceFail> {
+        self.is_fence_move_legal(player, axis, (x, y))?;
+        self.fences[y][x] = Some(axis);
+        match player {
+            PlayerColor::White => self.fences_left_for_white -= 1,
+            PlayerColor::Black => self.fences_left_for_black -= 1,
+        };
+        Ok(())
+    }
+
+    pub fn is_fence_move_legal(
+        &mut self,
+        player: PlayerColor,
+        axis: Axis,
+        (x, y): (usize, usize),
+    ) -> Result<(), AddFenceFail> {
+        use AddFenceFail::{Collides, NoFencesRemaining, NoPathRemaining};
         use Axis::{Horizontal, Vertical};
+
+        if 0 == match player {
+            PlayerColor::White => self.fences_left_for_white,
+            PlayerColor::Black => self.fences_left_for_black,
+        } {
+            return Err(NoFencesRemaining);
+        }
+
         if self.fences[y][x].is_some() {
             return Err(Collides);
         }
@@ -214,31 +289,164 @@ impl Board {
         match axis {
             Axis::Horizontal => {
                 if x > 0 && self.fences[y][x - 1] == Some(Horizontal)
-                    || x < 9 && self.fences[y][x + 1] == Some(Horizontal)
+                    || x < 7 && self.fences[y][x + 1] == Some(Horizontal)
                 {
                     return Err(Collides);
                 }
             }
             Axis::Vertical => {
                 if y > 0 && self.fences[y - 1][x] == Some(Vertical)
-                    || y < 9 && self.fences[y + 1][x] == Some(Vertical)
+                    || y < 7 && self.fences[y + 1][x] == Some(Vertical)
                 {
                     return Err(Collides);
                 }
             }
         }
-
-        todo!()
-        //TODOOOOO:check if there is a path
-
         self.fences[y][x] = Some(axis);
-
+        if !self.are_pawns_able_to_win() {
+            self.fences[y][x] = None;
+            return Err(NoPathRemaining);
+        }
+        self.fences[y][x] = None;
         Ok(())
+    }
+
+    pub fn are_pawns_able_to_win(&self) -> bool {
+        //dfs for the white pawn
+        'white_pawn_dfs: {
+            let mut stack = vec![self.white_pawn];
+            let mut visited = [[false; 9]; 9];
+            visited[self.white_pawn.1][self.white_pawn.0] = true;
+            while let Some((x, y)) = stack.pop() {
+                if y == 8 {
+                    break 'white_pawn_dfs;
+                }
+                for dir in [
+                    Direction::Down,
+                    Direction::Left,
+                    Direction::Right,
+                    Direction::Up,
+                ] {
+                    if self.is_obstructed((x, y), dir) {
+                        continue;
+                    }
+                    let (x1, y1) = dir.offset((x, y));
+                    if visited[y1][x1] {
+                        continue;
+                    }
+                    stack.push((x1, y1));
+                    visited[y1][x1] = true;
+                }
+            }
+            return false;
+        };
+        'black_pawn_dfs: {
+            let mut stack = vec![self.black_pawn];
+            let mut visited = [[false; 9]; 9];
+            visited[self.black_pawn.1][self.black_pawn.0] = true;
+            while let Some((x, y)) = stack.pop() {
+                if y == 0 {
+                    break 'black_pawn_dfs;
+                }
+                for dir in [
+                    Direction::Up,
+                    Direction::Left,
+                    Direction::Right,
+                    Direction::Down,
+                ] {
+                    if self.is_obstructed((x, y), dir) {
+                        continue;
+                    }
+                    let (x1, y1) = dir.offset((x, y));
+                    if visited[y1][x1] {
+                        continue;
+                    }
+                    stack.push((x1, y1));
+                    visited[y1][x1] = true;
+                }
+            }
+            return false;
+        };
+        true
+    }
+
+    pub const fn is_game_won(&self) -> Option<PlayerColor> {
+        if self.white_pawn.1 == 0 {
+            Some(PlayerColor::White)
+        } else if self.black_pawn.1 == 8 {
+            Some(PlayerColor::Black)
+        } else {
+            None
+        }
     }
 }
 
-const fn is_normal<T: Sized + Send + Sync + Unpin>() {}
+const fn is_nicely_send<T: Sized + Send + Sync + Unpin>() {}
 #[test]
 const fn normal_types() {
-    is_normal::<Board>();
+    is_nicely_send::<Board>();
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Move {
+    MovePlayer(Direction, Option<Direction>),
+    PlaceFence(Axis, (usize, usize)),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TryIntoMoveError {
+    UnrecognizedChar,
+    UnexpectedEndOfString,
+}
+impl TryFrom<String> for Move {
+    type Error = TryIntoMoveError;
+    fn try_from(value: String) -> Result<Self, TryIntoMoveError> {
+        use Axis::{Horizontal, Vertical};
+        use Direction::{Down, Left, Right, Up};
+        use Move::{MovePlayer, PlaceFence};
+        use TryIntoMoveError::{UnexpectedEndOfString, UnrecognizedChar};
+        let mut chars = value.chars();
+        let Some(first) = chars.next() else {
+            return Err(UnexpectedEndOfString);
+        };
+        match first.to_lowercase().next().ok_or(UnrecognizedChar)? {
+            dir @ ('w' | 'a' | 's' | 'd') => {
+                let second_dir = {
+                    match chars.next() {
+                        Some('w') => Some(Up),
+                        Some('a') => Some(Left),
+                        Some('s') => Some(Down),
+                        Some('d') => Some(Right),
+                        None => None,
+                        Some(_) => return Err(UnrecognizedChar),
+                    }
+                };
+                match dir {
+                    'w' => Ok(MovePlayer(Up, second_dir)),
+                    'a' => Ok(MovePlayer(Left, second_dir)),
+                    's' => Ok(MovePlayer(Down, second_dir)),
+                    'd' => Ok(MovePlayer(Right, second_dir)),
+                    _ => unreachable!(),
+                }
+            }
+            axis @ ('-' | 'h' | '|' | 'v') => {
+                let (Some(x_c), Some(y_c)) = (chars.next(), chars.next()) else {
+                    return Err(UnexpectedEndOfString);
+                };
+                let (x, y) = (x_c as usize - 'a' as usize, y_c as usize - '1' as usize);
+                if !(0..9).contains(&x) || !(0..9).contains(&y) {
+                    return Err(UnrecognizedChar);
+                }
+                Ok(PlaceFence(
+                    match axis {
+                        '-' | 'h' => Horizontal,
+                        '|' | 'v' => Vertical,
+                        _ => unreachable!(),
+                    },
+                    (x, y),
+                ))
+            }
+            _ => Err(UnrecognizedChar),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,68 @@
-#![allow(unused)]
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-use input_macro::input;
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::unwrap_used,
+    clippy::expect_used
+)]
+#![allow(dead_code)]
 mod game;
-use crate::game::{Axis, Board, Direction, MovePawnFail, PlayerColor};
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Move {
-    MovePlayer(Direction, Option<Direction>),
-    PlaceFence(Axis, usize, usize),
-}
-fn game_loop() {
-    use Axis::{Horizontal, Vertical};
-    use Direction::{Down, Left, Right, Up};
-    use Move::{MovePlayer, PlaceFence};
-    use PlayerColor::{Black, White};
 
+use crate::game::{AddFenceFail, Board, MoveMakeFail, PawnMoveFail, PlayerColor};
+
+fn game_loop() {
+    use PlayerColor::{Black, White};
     let mut turn = White;
     let mut board = Board::default();
     loop {
+        //'game loop
         println!("{board}");
-
+        //'trying_to_get_a_valid_move loop
         loop {
-            let str_move = input!("Type in {:?} player's move:", turn);
-            let mut chars_move = str_move.chars();
-            let Some(first) = chars_move.next() else {
-                println!("You didn't type anything");
-                continue;
-            };
-            let Some(the_move) = (match first.to_lowercase().next().unwrap_or(' ') {
-                dir @ ('w' | 'a' | 's' | 'd') => 'parse_pawn_move: {
-                    let second_dir = {
-                        match chars_move.next() {
-                            Some('w') => Some(Up),
-                            Some('a') => Some(Left),
-                            Some('s') => Some(Down),
-                            Some('d') => Some(Right),
-                            None => None,
-                            Some(_) => break 'parse_pawn_move None,
+            let try_into = input_macro::input!("Type in {turn} player's move:").try_into();
+            let the_move = match try_into {
+                Ok(the_move) => the_move,
+                Err(err) => {
+                    match err {
+                            game::TryIntoMoveError::UnrecognizedChar =>println!(
+                                "Couldn't understand the move, because there was an unrecognized character"),
+                            game::TryIntoMoveError::UnexpectedEndOfString =>println!(
+                                "Couldn't understand the move, because more characters were expected to be given"),
                         }
-                    };
-                    match dir {
-                        'w' => Some(MovePlayer(Up, second_dir)),
-                        'a' => Some(MovePlayer(Left, second_dir)),
-                        's' => Some(MovePlayer(Down, second_dir)),
-                        'd' => Some(MovePlayer(Right, second_dir)),
-                        _ => unreachable!(),
-                    }
+                    continue;
                 }
-                axis @ ('-' | 'h' | '|' | 'v') => 'parse_fence_move: {
-                    let (Some(x_c), Some(y_c)) = (chars_move.next(), chars_move.next()) else {
-                        break 'parse_fence_move None;
-                    };
-                    let (x, y) = (x_c as usize - 'a' as usize, y_c as usize - '1' as usize);
-                    if !(0..9).contains(&x) || !(0..9).contains(&y) {
-                        break 'parse_fence_move None;
-                    }
-                    Some(PlaceFence(
-                        match axis {
-                            '-' | 'h' => Horizontal,
-                            '|' | 'v' => Vertical,
-                            _ => unreachable!(),
-                        },
-                        x,
-                        y,
-                    ))
-                }
-                _ => None,
-            }) else {
-                println!("You didn't type a valid move string");
+            };
+            if let Err(err) = board.make_move(the_move, turn) {
+                match err {
+                    MoveMakeFail::PawnMoveFail(PawnMoveFail::PathObstructed) => println!(
+                        "Couldn't move the pawn, because the chosen path was obstructed"),
+                    MoveMakeFail::PawnMoveFail(PawnMoveFail::NoSecondaryDirection) => println!(
+                        "Couldn't move the pawn, because secondary direction was required but not provided"),
+                    MoveMakeFail::AddFenceFail(AddFenceFail::Collides) => println!(
+                        "Couldn't add the fence there, because it would collide with another fence"),
+                    MoveMakeFail::AddFenceFail(AddFenceFail::NoPathRemaining) => println!(
+                        "Couldn't add the fence there, because it would leave no path for at least one of the pawns"),
+                    MoveMakeFail::AddFenceFail(AddFenceFail::NoFencesRemaining) => println!(
+                        "Couldn't add the fence, because there are no fences left for you"
+                    )
+                };
                 continue;
-            };
-
-            println!("{the_move:?}");
-
-            match the_move {
-                MovePlayer(d, sd) => {
-                    if let Err(err) = board.move_pawn(turn, d, sd) {
-                        println!(
-                            "Couldn't make the move, because {}",
-                            match err {
-                                MovePawnFail::PathObstructed => "the choosen path was obstructed",
-                                MovePawnFail::NoSecondaryDirection =>
-                                    "secondary direction was required but not provided",
-                            }
-                        );
-                        continue;
-                    }
-                }
-                PlaceFence(axis, x, y) => {
-                    if let Err(err) = board.add_fence(axis, (x, y)) {
-                        println!(
-                            "Couldn't add the fence there, because it would {}",
-                            match err {
-                                game::AddFencelFail::Collides => "collide with another fence",
-                                game::AddFencelFail::NoPathRemaining =>
-                                    "leave no path for the other pawn",
-                            }
-                        );
-                        continue;
-                    }
-                }
-            };
+            }
             break;
         }
         turn = match turn {
             White => Black,
             Black => White,
+        };
+        if let Some(player) = board.is_game_won() {
+            println!("{board}");
+            println!("{player:?} player won!");
+            break;
         }
     }
 }
 
 fn main() {
     game_loop();
-    // let mut b = Board::default();
-    // b.fences[5][4] = Some(fence::Horizontal);
-    // println!("{b}");
+    // println!("{}",Board::default());
 }
